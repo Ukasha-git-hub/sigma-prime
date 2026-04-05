1b21b6ca1cf56885deede0fc4adebe0d69b6c0f9
ukasha khursheed <ukashakhursheed@gmail.com>
2026-04-05 11:30:02 +0500

    basic node command like exports and require and process.args etc

---
+++ BackEnd/math.js
@@ -0,0 +1,14 @@
+// module.exports.sum = (a,b)=> a+b;
+// module.exports.mul = (a,b) => a*b;
+// module.exports.g = 9.8;
+// module.exports.PI = 3.14;
+exports.sum = (a, b) => a + b;
+exports.mul = (a, b) => a * b;
+exports.g = 9.8;
+exports.PI = 3.14;
+// module.exports = {
+//     sum:sum,
+//     mul:mul,
+//     PI:PI,
+//     g:g
+// };
